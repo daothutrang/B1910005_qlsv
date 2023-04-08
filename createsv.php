659c4b7bf479328b3ac1cb7e063f14a586867a15
DAO THU TRANG <71755819+daothutrang@users.noreply.github.com>
2023-04-09 02:20:15 +0700

    upload

--- createsv.php
+++ createsv.php
@@ -1,32 +1,26 @@
 <?php
-$servername = "example-servername";
-$username = "example-username";
-$password = "example-password";
-$dbname = "example-dbname";
-
-// Tạo kết nối đến cơ sở dữ liệu
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Kết nối đến MySQL
+$conn = mysqli_connect("host", "username", "password", "database_name");
 
 // Kiểm tra kết nối
 if (!$conn) {
-	die("Kết nối không thành công: " . mysqli_connect_error());
+    die("Kết nối thất bại: " . mysqli_connect_error());
 }
 
-// Tạo table mới
+// Tạo bảng masv_cua_ban_qlsv
 $sql = "CREATE TABLE masv_cua_ban_qlsv (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-ho_ten VARCHAR(30) NOT NULL,
-nam_sinh INT(4) NOT NULL,
-email VARCHAR(50)
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    ho_ten VARCHAR(30) NOT NULL,
+    nam_sinh INT(4) NOT NULL,
+    email VARCHAR(50) NOT NULL
 )";
 
 if (mysqli_query($conn, $sql)) {
-	echo "Tạo table thành công";
+    echo "Tạo bảng masv_cua_ban_qlsv thành công";
 } else {
-	echo "Lỗi: " . mysqli_error($conn);
+    echo "Lỗi: " . mysqli_error($conn);
 }
 
+// Đóng kết nối
 mysqli_close($conn);
-
-echo "<br><a href='index.php'>Trở về trang chủ</a>";
 ?>
