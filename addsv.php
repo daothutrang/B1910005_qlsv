360b9d44d6e36f431eeab15a0fe8145f44513b78
DAO THU TRANG <71755819+daothutrang@users.noreply.github.com>
2023-04-09 02:35:04 +0700

    upload

--- addsv.php
+++ addsv.php
@@ -1,51 +1,49 @@
+<?php
+// Kết nối đến MySQL
+$conn = mysqli_connect("host", "username", "password", "database_name");
+
+// Kiểm tra kết nối
+if (!$conn) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
+}
+
+// Xử lý khi form được submit
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Lấy thông tin từ form
+    $hoTen = $_POST["ho_ten"];
+    $namSinh = $_POST["nam_sinh"];
+    $email = $_POST["email"];
+
+    // Tạo câu truy vấn SQL
+    $sql = "INSERT INTO masv_cua_ban_qlsv (ho_ten, nam_sinh, email) VALUES ('$hoTen', '$namSinh', '$email')";
+
+    // Thực hiện câu truy vấn
+    if (mysqli_query($conn, $sql)) {
+        echo "Thêm sinh viên thành công!";
+    } else {
+        echo "Thêm sinh viên thất bại: " . mysqli_error($conn);
+    }
+}
+
+// Đóng kết nối
+mysqli_close($conn);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Thêm sinh viên</title>
+    <meta charset="UTF-8">
+    <title>Thêm sinh viên mới</title>
 </head>
 <body>
-	<h1>Thêm sinh viên</h1>
-	<form action="addsv.php" method="POST">
-		<label for="ho_ten">Họ tên:</label>
-		<input type="text" name="ho_ten" id="ho_ten" required><br><br>
-		<label for="nam_sinh">Năm sinh:</label>
-		<input type="number" name="nam_sinh" id="nam_sinh" required><br><br>
-		<label for="email">Email:</label>
-		<input type="email" name="email" id="email"><br><br>
-		<input type="submit" value="Thêm">
-	</form>
-
-	<?php
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		// Lấy dữ liệu từ form
-		$ho_ten = $_POST['ho_ten'];
-		$nam_sinh = $_POST['nam_sinh'];
-		$email = $_POST['email'];
-
-		// Kết nối đến MySQL
-		$conn = mysqli_connect("host", "username", "password", "database_name");
-
-		// Kiểm tra kết nối
-		if (!$conn) {
-		    die("Kết nối thất bại: " . mysqli_connect_error());
-		}
-
-		// Thêm sinh viên vào cơ sở dữ liệu
-		$sql = "INSERT INTO masv_cua_ban_qlsv (ho_ten, nam_sinh, email)
-				VALUES ('$ho_ten', '$nam_sinh', '$email')";
-
-		if (mysqli_query($conn, $sql)) {
-		    echo "Thêm sinh viên thành công";
-            else {
-                echo "Thêm sinh viên thất bại: " . mysqli_error($conn);
-            }
-    
-            // Đóng kết nối
-            mysqli_close($conn);
-        }
-        ?>
-    
-        <a href="index.php">Quay lại trang chủ</a>
-    </body>
-    </html>
-    
+    <h1>Thêm sinh viên mới</h1>
+    <form method="POST">
+        Họ tên: <input type="text" name="ho_ten"><br>
+        Năm sinh: <input type="text" name="nam_sinh"><br>
+        Email: <input type="text" name="email"><br>
+        <input type="submit" value="Thêm">
+    </form>
+    <br>
+    <a href="index.php">Quay lại trang chủ</a>
+</body>
+</html>
