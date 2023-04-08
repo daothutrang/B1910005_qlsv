556b77f0d5de2de4267f01a49a840f0c948d5195
DAO THU TRANG <71755819+daothutrang@users.noreply.github.com>
2023-04-09 02:29:49 +0700

    upload

--- createsv.php
+++ createsv.php
@@ -1,5 +1,3 @@
-
-mysql://uals7ougv4ckk1zr:GNYP7pM6kJ6Yu0mcfiFz@btz6puevibn8o70fouji-mysql.services.clever-cloud.com:3306/btz6puevibn8o70fouji
 <?php
 // Kết nối đến MySQL
 $conn = mysqli_connect("host", "username", "password", "database_name");
@@ -12,9 +10,9 @@ if (!$conn) {
 // Tạo bảng masv_cua_ban_qlsv
 $sql = "CREATE TABLE masv_cua_ban_qlsv (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    ho_ten VARCHAR(30) NOT NULL,
+    ho_ten VARCHAR(50) NOT NULL,
     nam_sinh INT(4) NOT NULL,
-    email VARCHAR(50) NOT NULL
+    email VARCHAR(70) NOT NULL UNIQUE
 )";
 
 if (mysqli_query($conn, $sql)) {
@@ -25,4 +23,6 @@ if (mysqli_query($conn, $sql)) {
 
 // Đóng kết nối
 mysqli_close($conn);
+
+echo "<br><a href='index.php'>Quay lại trang chủ</a>";
 ?>
