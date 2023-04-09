844a8005a033a8741ea9e1db8a2cf7e54b00d1d4
DAO THU TRANG <71755819+daothutrang@users.noreply.github.com>
2023-04-09 10:37:56 +0700

    upload

--- addsv.php
+++ addsv.php
@@ -5,18 +5,18 @@
         Insert Student
 
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="border">
-            ID<BR> <input type="text" name="id" value ="<?php echo $id; ?>"><BR>
-            Full Name<BR> <input type="text" name="ho_ten" value ="<?php echo $ho_ten;?>"><BR>
-            Year of Birth<BR> <input type="text" name="nam_sinh" value ="<?php echo $nam_sinh ?>"><BR>
-            Email<BR> <input type="text" name="email" value ="<?php echo $email ?>"><BR>
+            ID<BR> <input type="number" name="id" value =""><BR>
+            Full Name<BR> <input type="text" name="name" value =""><BR>
+            Year of Birth<BR> <input type="number" name="year_of_birth" value =""><BR>
+            Email<BR> <input type="text" name="email" value =""><BR>
             <input type="submit" name="submit" value="submit">
         </form> 
 
         <?php
             if($_SERVER["REQUEST_METHOD"] == "POST") {
                 $id = $_POST["id"];
-                $ho_ten = $_POST["ho_ten"];
-                $nam_sinh = $_POST["nam_sinh"];
+                $name = $_POST["name"];
+                $year_of_birth = $_POST["year_of_birth"];
                 $email = $_POST["email"];
     
                 $MYSQL_ADDON_HOST = getenv('MYSQL_ADDON_HOST');
@@ -33,7 +33,7 @@
                     echo "\nConnected to database successfully.\n";
                 }
     
-                $sql = "INSERT INTO masv_cua_ban_qlsv (id, ho_ten, nam_sinh, email) VALUES ('$id', '$ho_ten', '$nam_sinh', '$email')";
+                $sql = "INSERT INTO masv_cua_ban_qlsv (id, ho_ten, nam_sinh, email) VALUES ('$id', '$name', '$year_of_birth', '$email')";
     
                 if (mysqli_query($conn, $sql)) {
                     echo "\nStudent added successfully.";
@@ -44,6 +44,7 @@
                 mysqli_close($conn);
             }
         ?>
-             <a href="index.php">Go back to homepage</a>
+        
+        <a href="index.php">Back to index</a>
     </body>
 </html>
