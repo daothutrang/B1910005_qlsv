8e1b0d5e031a1f8ac4b15c6b6aed915eb66f4cef
DAO THU TRANG <71755819+daothutrang@users.noreply.github.com>
2023-04-09 10:40:37 +0700

    upload

--- listsv.php
+++ listsv.php
@@ -8,32 +8,28 @@
             $MYSQL_ADDON_USER = getenv('MYSQL_ADDON_USER');
             $MYSQL_ADDON_PASSWORD = getenv('MYSQL_ADDON_PASSWORD');
     
-            $conn = mysqli_connect($MYSQL_ADDON_HOST, $MYSQL_ADDON_USER, $MYSQL_ADDON_PASSWORD, $MYSQL_ADDON_DB);
-    
+                        $conn = mysqli_connect($MYSQL_ADDON_HOST, $MYSQL_ADDON_USER, $MYSQL_ADDON_PASSWORD, $MYSQL_ADDON_DB);
             if (!$conn) {
                 echo "<p>Error: Unable to connect to database.</p>";
             } else {
                 echo "<p>Connected to database successfully.</p>";
-    
-                $sql = "SELECT * FROM students";
-    
+                $sql = "SELECT * FROM users";
                 $result = mysqli_query($conn, $sql);
-    
                 if (mysqli_num_rows($result) > 0) {
-                    echo "<table>";
-                    echo "<tr><th>ID</th><th>Name</th><th>Email</th></tr>";
                     while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
+                        echo "<tr>";
+                        echo "<td>" . $row["id"] . "</td>";
+                        echo "<td>" . $row["name"] . "</td>";
+                        echo "<td>" . $row["email"] . "</td>";
+                        echo "</tr>";
                     }
-                    echo "</table>";
                 } else {
                     echo "<p>No results found.</p>";
                 }
-    
                 mysqli_close($conn);
             }
         ?>
-        <br>
-        <a href="index.php">Return to index page</a>
+    </table>
+    <a href="index.php">Back to index</a>
     </body>
 </html>
